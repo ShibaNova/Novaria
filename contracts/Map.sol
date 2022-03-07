1abdfda9886c0532bf226267d023b8671b32ed57
Jonathan <80490016+cryptodev999@users.noreply.github.com>
2022-03-07 09:20:26 -0600

    Update Map.sol
    Switch time modifier to 1.

--- contracts/Map.sol
+++ contracts/Map.sol
@@ -33,7 +33,7 @@ contract Map is Editor {
         _travelCooldownPerDistance = 900; //15 minutes
         _maxTravel = 5; //AU
         _rewardsTimer = 0;
-        _timeModifier = 5;
+        _timeModifier = 1;
         _miningCooldown = 1800; //30 minutes
         _minTravelSize = 25;
         _collectCooldownReduction = 5;
