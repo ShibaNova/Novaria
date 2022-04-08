8c5df3f977a42992b9cd7db2d00dc7e01b6ee8d9
n8urm8 <81867790+n8urm8@users.noreply.github.com>
2022-04-08 14:03:43 -0400

    new referral contract

---
+++ contracts/Referrals.sol
@@ -0,0 +1,78 @@
+pragma solidity ^0.8.7;
+
+
+import "./libs/ShibaBEP20.sol";
+import "./libs/SafeBEP20.sol";
+import "./libs/Editor.sol";
+
+interface IFleet {
+    function playerExists(address _player) external view returns (bool);
+}
+
+contract Referrals is Editor {
+    using SafeBEP20 for ShibaBEP20;
+
+    constructor(
+        ShibaBEP20 _Token
+        
+    ) {
+        Token = _Token;
+        referralRate = 25000000000000000000;
+    }
+
+    ShibaBEP20 public Token; // nova token address
+    IFleet public Fleet; // fleet contract
+    uint public referralRate; // how much token per referral
+
+    mapping (address => bool) addressReferred; // checks if referral is already registered
+    mapping (address => bool) referralAddressPaid;
+    mapping (address => address) public getReferrer; 
+    mapping (address => uint) public referralBalance; // current generated balance minus previously withdrawn
+    mapping (address => address[]) public referralsByAddress;
+
+    // Step 1 - new user has to use this function to approve the referrer before joining the game
+    function addReferral (address _referrer) external {
+        address referred = msg.sender;
+        require(addressReferred[referred] == false, 'REFERRALS: USER ALREADY REGISTERED REFERRAL');
+        require(Fleet.playerExists(referred) == false, 'REFERRALS: USER ALREADY REGISTERED');
+        addressReferred[referred] = true;
+        getReferrer[referred] = _referrer;
+        referralsByAddress[_referrer].push(referred);
+    }
+
+    function checkReferrals (address _referrer) external view returns(uint) {
+        uint pendingReferrals;
+        for (uint i = 0; i < referralsByAddress[_referrer].length; i++) {
+            if (Fleet.playerExists(referralsByAddress[_referrer][i]) == true && referralAddressPaid[referralsByAddress[_referrer][i]] == false) {
+                pendingReferrals++;
+            }
+        }
+    }
+
+    function getReferralBonus () external {
+        address referrer = msg.sender;
+        uint pendingReferrals;
+        // Check if a referred wallet has bought the game
+        for (uint i = 0; i < referralsByAddress[referrer].length; i++) {
+            if (Fleet.playerExists(referralsByAddress[referrer][i]) == true && referralAddressPaid[referralsByAddress[referrer][i]] == false) {
+                pendingReferrals++;
+                referralAddressPaid[referralsByAddress[referrer][i]] = true;           
+            }
+        }
+        // Clean up addresses that bought in, doesn't have to be perfect
+        for (uint i = 0; i < referralsByAddress[referrer].length; i++) {
+            if (referralAddressPaid[referralsByAddress[referrer][i]]) {
+                referralsByAddress[referrer][i] = referralsByAddress[referrer][referralsByAddress[referrer].length-1];
+                referralsByAddress[referrer].pop();
+            }
+        }
+        require(Token.balanceOf(address(this)) >= (referralRate * pendingReferrals), 'REFERRAL: Not enough token to pay out referrals, contact Dev team');
+        Token.safeTransfer(referrer, (referralRate * pendingReferrals));
+    }
+
+    // be sure to use appropriate decimals
+    function setReferralRate (uint _newRate) external onlyOwner {
+        referralRate = _newRate; 
+    }
+
+}
